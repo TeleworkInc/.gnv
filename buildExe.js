16c008b7872156771411d9da0025dc4e8b40ef9e
Christian Lewis <ctjl96@gmail.com>
2020-08-18 18:22:51 -0500

    Formatting

--- buildExe.js
+++ buildExe.js
@@ -11,13 +11,13 @@ import shell from 'await-shell';
 (async () => {
   await shell(
       `google-closure-compiler
-          --entry_point dev/universal.mjs
-          -O ADVANCED
-          --process_common_js_modules
-          --module_resolution NODE
-          --dependency_mode PRUNE
-          --js $(npm root -g)/google-closure-library/closure/goog/base.js
-          --js dev/universal.mjs
-          --js_output_file dist/exe.js`,
+        --entry_point dev/universal.mjs
+        -O ADVANCED
+        --process_common_js_modules
+        --module_resolution NODE
+        --dependency_mode PRUNE
+        --js $(npm root -g)/google-closure-library/closure/goog/base.js
+        --js dev/universal.mjs
+        --js_output_file dist/exe.js`,
   );
 })();
