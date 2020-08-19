7cb04e9ed541b38b406ac02b914cc991d318bb23
Christian Lewis <ctjl96@gmail.com>
2020-08-19 17:28:05 -0500

    Added static `import.meta.url`

--- rollup.dev.es.js
+++ rollup.dev.es.js
@@ -3,47 +3,53 @@
  */
 /**
  * @fileoverview
- * Rollup ES dev config.
+ * Rollup ES dev config. Special considerations are added, such as named exports
+ * as default and `import.meta.url` resolution.
  */
 
 import glob from 'glob';
 import exportDefault from 'rollup-plugin-export-default';
 import { defaultPlugins } from './rollup.plugins.js';
 import { devExternal } from './rollup.externals.js';
+import importMetaUrl from 'rollup-plugin-import-meta-url';
 
-const exportESM = (file) => {
-  return {
-    input: file,
-    output: {
-      file: file
-          .replace('exports', 'dev')
-          .replace('.js', '.mjs'),
-      format: 'esm',
-      // will help with compiler inlining
-      preferConst: true,
-    },
-    plugins: [
-      ...defaultPlugins,
-      exportDefault(),
-    ],
-    external: devExternal,
-  };
-};
+const exportESM = (file) => ({
+  input: file,
+  output: {
+    file: file
+        .replace('exports', 'dev')
+        .replace('.js', '.mjs'),
+    format: 'esm',
+    // will help with compiler inlining
+    preferConst: true,
+  },
+  plugins: [
+    ...defaultPlugins,
+    /**
+     * Handle `import.meta.url` in dev ESM output. Resolve it the static
+     * absolute path of the source file.
+     */
+    importMetaUrl(),
+    /**
+     * Export named exports as default if no default export defined.
+     */
+    exportDefault(),
+  ],
+  external: devExternal,
+});
 
-const exportCJS = (file) => {
-  return {
-    input: file,
-    output: {
-      file: file
-          .replace('exports', 'dev')
-          .replace('.js', '.cjs'),
-      format: 'cjs',
-      preferConst: true,
-    },
-    plugins: defaultPlugins,
-    external: devExternal,
-  };
-};
+const exportCJS = (file) => ({
+  input: file,
+  output: {
+    file: file
+        .replace('exports', 'dev')
+        .replace('.js', '.cjs'),
+    format: 'cjs',
+    preferConst: true,
+  },
+  plugins: defaultPlugins,
+  external: devExternal,
+});
 
 export default [
   /**
