1c9b7e6baa646cee133c97b52ad98e0c54527dec
Christian Lewis <ctjl96@gmail.com>
2020-08-24 17:07:57 -0500

    Refactor

--- boot.js
+++ boot.js
@@ -9,37 +9,12 @@
  * @author Christian Lewis <hello@trytelework.com>
  */
 
-import { readPackageJson } from '../package.js';
-import { spawnSync } from 'child_process';
-
-const packageJson = readPackageJson();
-
-const versionString = (deps = {}) => (
-  Object.entries(deps || {}).map(
-      ([key, val]) => `${key}@${val}`,
-  )
-);
-
-const spacer = (str) => console.log(
-    '\x1b[96m%s\x1b[0m', `[𝓰𝓷𝓿]` + ` ${str}`,
-);
-
-const gnvDependencies = versionString(packageJson.gnvDependencies);
-const peerDependencies = versionString(packageJson.peerDependencies);
-
-const callNpm = (...args) => {
-  console.log(`\n> npm ${args.join(' ')}\n`);
-  spawnSync(
-      'npm',
-      args,
-      {
-        /**
-         * Only inherit stderr.
-         */
-        stdio: ['ignore', 'ignore', 'inherit'],
-      },
-  );
-};
+import {
+  spacer,
+  callNpm,
+  installPeerDependencies,
+  installGnvDependencies,
+} from '../package.js';
 
 /** Remove visual clutter. */
 console.log();
@@ -57,37 +32,11 @@ callNpm('link', '-f', '--no-save', '--silent');
 /**
  * Install gnvDependencies in this folder without updating package.json.
  */
-if (gnvDependencies.length) {
-  spacer('Adding local gnv deps to node_modules:');
-  callNpm('i', '-f', '--no-save', '--silent', ...gnvDependencies);
-}
+installGnvDependencies();
 
 
 /**
  * Globally install all peerDependencies without updating package.json, then
  * link all globally installed peerDeps to make them available in this package.
  */
-if (peerDependencies.length) {
-  /**
-   * Make sure no previous versions of this package are linked in this
-   * workspace.
-   */
-  const anyVersionPeerDeps = Object.keys(packageJson.peerDependencies);
-
-  /**
-   * Install peerDeps globally.
-   */
-  spacer('Adding global peerDeps:');
-  callNpm('i', '-g', '--no-save', ...peerDependencies);
-
-  /**
-   * Link peerDeps locally. Also links this package so that CLIs are available.
-   */
-  spacer('Linking peer dependencies locally...');
-  callNpm('link', '-f', '--no-save', ...anyVersionPeerDeps);
-
-  /**
-   * Everything was successful!
-   */
-  spacer('Done! Your development CLI should be ready at `gnv-dev`.\n');
-}
+installPeerDependencies();
