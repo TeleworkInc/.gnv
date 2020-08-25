7c0dd81206369c78821b75d1c9bf8f8c031e5207
Christian Lewis <ctjl96@gmail.com>
2020-08-25 00:22:50 -0500

    Dependency-free buildExe.js

--- npm/buildExe.js
+++ npm/buildExe.js
@@ -3,21 +3,25 @@
  */
 /**
  * @fileoverview
- * Build the exe output.
+ * Build the exe output. Use builtins only to avoid adding peerDeps.
  */
 
-import shell from 'await-shell';
+import { spawnSync } from 'child_process';
 
-(async () => {
-  await shell(
-      `google-closure-compiler
-        --entry_point dev/universal.mjs
-        -O ADVANCED
-        --process_common_js_modules
-        --module_resolution NODE
-        --dependency_mode PRUNE
-        --js $(npm root -g)/google-closure-library/closure/goog/base.js
-        --js dev/universal.mjs
-        --js_output_file dist/exe.js`,
-  );
-})();
+spawnSync(
+    'google-closure-compiler',
+    [
+      '--entry_point dev/universal.mjs',
+      '-O ADVANCED',
+      '--process_common_js_modules',
+      '--module_resolution NODE',
+      '--dependency_mode PRUNE',
+      '--js $(npm root -g)/google-closure-library/closure/goog/base.js',
+      '--js dev/universal.mjs',
+      '--js_output_file dist/exe.js',
+    ],
+    {
+      shell: true,
+      stdio: 'inherit',
+    },
+);
