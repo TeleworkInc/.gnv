3e8c77b2306d1a5bbf65eda8479c6bb1c701bcc6
Christian Lewis <ctjl96@gmail.com>
2020-08-24 19:57:58 -0500

    Moved boot to package.js

--- boot.js
+++ boot.js
@@ -9,34 +9,5 @@
  * @author Christian Lewis <hello@trytelework.com>
  */
 
-import {
-  spacer,
-  callNpm,
-  installPeerDependencies,
-  installGnvDependencies,
-} from '../package.js';
-
-/** Remove visual clutter. */
-console.log();
-
-
-/**
- * Link this package. This has to be done before everything else due to the
- * weird behavior of npm, which will delete necessary dependencies if this is
- * run after installing peerDeps or gnvDeps.
- */
-spacer('Linking this package to global bin...');
-callNpm('link', '-f', '--no-save', '--silent');
-
-
-/**
- * Install gnvDependencies for the package.json in the parent folder.
- */
-installGnvDependencies(true);
-
-
-/**
- * Install peerDependencies for the package.json in the parent folder, and link
- * into local `node_modules`.
- */
-installPeerDependencies(true);
+import { boot } from '../package.js';
+(async () => await boot())();
