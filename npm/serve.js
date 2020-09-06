dcc415c50824e8191d27bb61f516958e5cd6d7a1
Christian Lewis <ctjl96@gmail.com>
2020-09-06 18:35:09 -0500

    Updated dev url

--- npm/serve.js
+++ npm/serve.js
@@ -6,5 +6,5 @@ const port = 42069;
 
 app.use(express.static(process.cwd()));
 app.listen(port, () => {
-  console.log(`Dev at: http://localhost:${port}/widget.dev.html`);
+  console.log(`Dev at: http://localhost:42069/widget.render.dev.html`);
 });
